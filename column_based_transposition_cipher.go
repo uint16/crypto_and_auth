410d0dc595f11b6e1f320b24d048c8bf74b61baf
Damas <damagination@users.noreply.github.com>
2016-02-08 23:18:43 -0500

    Added functionality to display matrix

--- column_based_transposition_cipher.go
+++ column_based_transposition_cipher.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 func encrypt() {
 
 }
@@ -8,6 +13,34 @@ func decrypt() {
 
 }
 
-func main() {
+func showMatrix(s string) {
+	length := len(s)
+	col := int(math.Ceil(math.Sqrt(float64(length))))
+	row := int(math.Ceil(float64(length) / float64(col)))
+	matrix := make([][]string, row)
+	for x := range matrix {
+		matrix[x] = make([]string, col)
+	}
+	l := 0
+	for i := 0; i < row; i++ {
+		for j := 0; j < col; j++ {
+			if length > l {
+				matrix[i][j] = string(s[l])
+			} else {
+				matrix[i][j] = "\\"
+			}
+			fmt.Print(matrix[i][j], " ")
+			l++
+		}
+		fmt.Println()
+	}
+}
 
+func main() {
+	var plainText string
+	//	var cipherText, key string
+	fmt.Println("Enter text to encrypt: ")
+	fmt.Scanln(&plainText)
+
+	showMatrix(plainText)
 }
